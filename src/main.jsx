94c91f4683cd324004982e19c29c281ce76faf8a
Pathivada-Varshini <varshinipathivada20@gmail.com>
2025-06-23 22:46:49 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './styles.css'; 
+import App from './App.jsx'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
